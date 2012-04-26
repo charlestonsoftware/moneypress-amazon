c863e934ac5100be553361da7547909a6f6278ad
Lance Cleveland <lance@cybersprocket.com>
2012-04-25 23:46:50 -0400

    Add Pro Pack add-on.

--- include/config.php
+++ include/config.php
@@ -15,16 +15,22 @@ if (defined('MP_AMZ_PLUGINDIR')) {
    
     $MP_amz_plugin = new wpCSL_plugin__mpamz(
         array(
-            'use_obj_defaults'      => true,        
+            'use_obj_defaults'      => true,   
+            'cache_obj_name'        => 'mpamzcache',            
+            
             'prefix'                => MP_AMZ_PREFIX,
             'css_prefix'            => 'csl_themes',            
             'name'                  => 'MoneyPress : Amazon Edition',
+            
             'url'                   => 'http://www.cybersprocket.com/products/moneypress-amazon/',
-            'paypal_button_id'      => 'LUJK7AZN7MRDJ',
+            'support_url'           => 'http://redmine.cybersprocket.com/projects/moneypress-amazon/wiki',
+            'purchase_url'          => 'http://www.cybersprocket.com/products/moneypress-amazon/',
+            
             'basefile'              => MP_AMZ_BASENAME,
             'plugin_path'           => MP_AMZ_PLUGINDIR,
             'plugin_url'            => MP_AMZ_PLUGINURL,
             'cache_path'            => MP_AMZ_PLUGINDIR . 'cache',
+            
             'driver_name'           => 'Amazon',
             'driver_type'           => 'Panhandler',
             'driver_args'           => array(
@@ -34,10 +40,57 @@ if (defined('MP_AMZ_PLUGINDIR')) {
                     'associatetag'        => get_option(MP_AMZ_PREFIX.'-AssociateTag'),
                     'searchindex'         => get_option(MP_AMZ_PREFIX.'-SearchIndex'),
                     ),
-            'shortcodes'            => array('mp-amz','MP-AMZ','mp-amazon'),
-            'themes_enabled'        => true            
+            'shortcodes'            => array('mpamz', 'mp-amz','MP-AMZ','mp-amazon'),
+            
+            'has_packages'          => true,
         )
     );    
-}    
-    
+    // Setup our optional packages
+    //
+    add_options_packages_for_mpamz();        
 
+}
+
+
+/**************************************
+ ** function: add_options_packages_for_mpamz
+ **
+ ** Setup the option package list.
+ **/
+function add_options_packages_for_mpamz() {
+    configure_mpamz_propack();
+}
+
+
+/**************************************
+ ** function: configure_mpamz_propack
+ **
+ ** Configure the Pro Pack.
+ **/
+function configure_mpamz_propack() {
+    global $MP_amz_plugin;
+   
+    // Setup metadata
+    //
+    $MP_amz_plugin->license->add_licensed_package(
+            array(
+                'name'              => 'Pro Pack',
+                'help_text'         => 'A variety of enhancements are provided with this package.  ' .
+                                       'See the <a href="'.$MP_amz_plugin->purchase_url.'" target="Cyber Sprocket">product page</a> for details.  If you purchased this add-on ' .
+                                       'come back to this page to enter the license key to activate the new features.',
+                'sku'               => 'MPAMZ',
+                'paypal_button_id'  => 'LUJK7AZN7MRDJ',
+                'paypal_upgrade_button_id' => 'LUJK7AZN7MRDJ'
+            )
+        );
+    
+    // Enable Features Is Licensed
+    //
+    if ($MP_amz_plugin->license->packages['Pro Pack']->isenabled_after_forcing_recheck()) {
+             //--------------------------------
+             // Enable Themes
+             //
+             $MP_amz_plugin->themes_enabled = true;
+             $MP_amz_plugin->themes->css_dir = MP_AMZ_PLUGINDIR . 'css/';
+    }        
+}
--- plus.php
+++ plus.php
@@ -2,7 +2,6 @@
 /****************************************************************************
  ** file: plus.php
  **
- ** Things that are not part of the LE product
  ***************************************************************************/
 
  
@@ -13,90 +12,5 @@
  **
  **/
 function csl_mpamz_add_settings() {
-    global $MP_amz_plugin;
-    
-    
-    // The Themes
-    // No themes? Force the default at least
-    //
-    $themeArray = get_option(MP_AMZ_PREFIX.'-theme_array');
-    if (count($themeArray, COUNT_RECURSIVE) <= 2) {
-        $themeArray = array('Off-White Single Column' => 'mp-offwhite');
-    }    
-
-    // Check for theme files
-    //
-    $lastNewThemeDate = get_option(MP_AMZ_PREFIX.'-theme_lastupdated');
-    $newEntry = array();
-    if ($dh = opendir(MP_AMZ_COREDIR.'css/')) {
-        while (($file = readdir($dh)) !== false) {
-            
-            // If not a hidden file
-            //
-            if (!preg_match('/^\./',$file)) {                
-                $thisFileModTime = filemtime(MP_AMZ_COREDIR.'css/'.$file);
-                
-                // We have a new theme file possibly...
-                //
-                if ($thisFileModTime > $lastNewThemeDate) {
-                    $newEntry = csl_mpamz_GetThemeInfo(MP_AMZ_COREDIR.'css/'.$file);
-                    $themeArray = array_merge($themeArray, array($newEntry['label'] => $newEntry['file']));                                        
-                    update_option(MP_AMZ_PREFIX.'-theme_lastupdated', $thisFileModTime);
-                }
-            }
-        }
-        closedir($dh);
-    }
-    
-    // We added at least one new theme
-    //
-    if (count($newEntry, COUNT_RECURSIVE) > 1) {
-        update_option(MP_AMZ_PREFIX.'-theme_array',$themeArray);
-    }  
-        
-    $MP_amz_plugin->settings->add_item(
-        'Product Display', 
-        'Select A Theme',   
-        'theme',    
-        'list', 
-        false, 
-        'How should the plugin UI elements look?',
-        $themeArray
-    );
 }
 
-/**************************************
- ** function: csl_mpamz_GetThemeInfo
- ** 
- ** Extract the label & key from a CSS file header.
- **
- **/
-function csl_mpamz_GetThemeInfo ($filename) {    
-    $dataBack = array();
-    if ($filename != '') {
-       $default_headers = array(
-            'label' => 'label',
-            'file' => 'file',
-            'columns' => 'columns'
-           );
-        
-       $dataBack = get_file_data($filename,$default_headers,'');
-       $dataBack['file'] = preg_replace('/.css$/','',$dataBack['file']);       
-    }
-    
-    return $dataBack;
- }
- 
- 
-/**************************************
- ** function: csl_mpamz_configure_theme
- ** 
- ** Configure the plugin theme drivers based on the theme file meta data.
- **
- **/
- function csl_mpamz_configure_theme($themeFile) {
-    global $MP_amz_plugin;
-    
-    $newEntry = csl_mpamz_GetThemeInfo(MP_AMZ_COREDIR.$themeFile);
-    $MP_amz_plugin->products->columns = $newEntry['columns'];
- }
